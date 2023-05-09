e943d438ff46fcb2b629b0fb6d1568b2ab9be739
yrysov-b <yrysov_b@auca.kg>
2023-05-10 01:15:40 +0600

    gen server version of todo-list

--- lib/todo_list.ex
+++ lib/todo_list.ex
@@ -1,3 +1,39 @@
+defmodule TodoServer do
+  use GenServer
+
+  def start do
+    GenServer.start(TodoServer, nil)
+  end
+
+  def add_entry(todo_server, new_entry) do
+    GenServer.cast(todo_server, {:add_entry, new_entry})
+  end
+
+  def entries(todo_server, date) do
+    GenServer.call(todo_server, {:entries, date})
+  end
+
+   @impl GenServer
+   def init(_) do
+     {:ok, TodoList.new()}
+   end
+
+    @impl GenServer
+    def handle_cast({:add_entry, new_entry}, todo_list) do
+      new_state = TodoList.add_entry(todo_list, new_entry)
+      {:noreply, new_state}
+    end
+
+    @impl GenServer
+    def handle_call({:entries, date}, _, todo_list) do
+      {
+        :reply,
+        TodoList.entries(todo_list, date),
+        todo_list
+      }
+    end
+end
+
 defmodule TodoList do
   defstruct auto_id: 1, entries: %{}
 
--- test/todo_list_test.exs
+++ test/todo_list_test.exs
@@ -2,33 +2,13 @@ defmodule TodoListTest do
   use ExUnit.Case
   doctest TodoList
 
-  test "create a new todo List" do
-    assert TodoList.new() == %TodoList{}
-  end
+  test "todo_server" do
+    {:ok, pid} = TodoServer.start()
+    TodoServer.add_entry(pid, %{date: ~D[2018-01-01], title: "Dinner"})
+    TodoServer.add_entry(pid, %{date: ~D[2018-01-02], title: "Dentist"})
+    TodoServer.add_entry(pid, %{date: ~D[2018-01-02], title: "Meeting"})
 
-
-  test "get entries for a date" do
-    todo_list = TodoList.new()
-    |> TodoList.add_entry(%{date: {2013, 12, 19}, title: "Dentist"})
-
-    assert TodoList.entries(todo_list, {2013, 12, 19}) ==  [%{date: {2013, 12, 19}, id: 1, title: "Dentist"}]
-  end
-
-  test "todo_crud" do
-    list =
-      TodoList.new()
-      |> TodoList.add_entry(%{date: ~D[2018-01-01], title: "Dinner"})
-      |> TodoList.add_entry(%{date: ~D[2018-01-02], title: "Dentist"})
-      |> TodoList.add_entry(%{date: ~D[2018-01-02], title: "Meeting"})
-
-    assert [%{date: ~D[2018-01-01], id: 1, title: "Updated"}] ==
-             list
-             |> TodoList.update_entry(1, fn entry -> %{entry | title: "Updated"} end)
-             |> TodoList.entries(~D[2018-01-01])
-
-    assert [] ==
-             list
-             |> TodoList.delete_entry(1)
-             |> TodoList.entries(~D[2018-01-01])
+    assert [%{date: ~D[2018-01-01], id: 1, title: "Dinner"}] ==
+             TodoServer.entries(pid, ~D[2018-01-01])
   end
 end
