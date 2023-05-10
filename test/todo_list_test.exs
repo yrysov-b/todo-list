8d9516734c3308546a6691913e924b8f6e998d9c
yrysov-b <yrysov_b@auca.kg>
2023-05-10 11:57:04 +0600

    changed mix structure

--- test/todo_list_test.exs
+++ test/todo_list_test.exs
@@ -1,14 +1,14 @@
 defmodule TodoListTest do
   use ExUnit.Case
-  doctest TodoList
+  doctest Todo
 
   test "todo_server" do
     {:ok, pid} = TodoServer.start()
-    TodoServer.add_entry(pid, %{date: ~D[2018-01-01], title: "Dinner"})
-    TodoServer.add_entry(pid, %{date: ~D[2018-01-02], title: "Dentist"})
-    TodoServer.add_entry(pid, %{date: ~D[2018-01-02], title: "Meeting"})
+    Todo.Server.add_entry(pid, %{date: ~D[2018-01-01], title: "Dinner"})
+    Todo.Server.add_entry(pid, %{date: ~D[2018-01-02], title: "Dentist"})
+    Todo.Server.add_entry(pid, %{date: ~D[2018-01-02], title: "Meeting"})
 
     assert [%{date: ~D[2018-01-01], id: 1, title: "Dinner"}] ==
-             TodoServer.entries(pid, ~D[2018-01-01])
+             Todo.Server.entries(pid, ~D[2018-01-01])
   end
 end
